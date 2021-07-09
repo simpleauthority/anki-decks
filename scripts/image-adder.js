de24e84a6b2c1d5fffbc43420ccb82ba887a8c49
simpleauthority <jacob@algorithmjunkie.com>
2021-07-09 15:52:29 -0700

    associate images with words using script

--- scripts/image-adder.js
+++ scripts/image-adder.js
@@ -1,36 +1,45 @@
 import * as fs from 'fs/promises'
 
-const pad = num => {
-    return String(num).padStart(5, '0')
-}
-
 (async () => {
     let handle
-    try {
-        console.log('Opening first file...')
-        handle = await fs.open('./words-with-tagalog.txt')
 
-        console.log('Reading first file...')
+    try {
+        let filename = process.argv[2]
+        if (!filename) {
+            console.error("No words file given!")
+            return
+        }
+
+        let targetFilename = process.argv[3]
+        if (!targetFilename) {
+            console.error("No target filename given!")
+            return
+        }
+
+        let imagePath = process.argv[4]
+        if (!imagePath) {
+            console.error("No image path given!")
+            return
+        }
+
+        console.log(`Reading words file ${filename}...`)
+        handle = await fs.open(filename)
         let content = await handle.readFile({ encoding: 'utf-8' })
         content = content.split("\n")
-
-        console.log('Closing first file...')
         await handle.close()
 
-        console.log('First file closed. Performing in-memory replacements...')
-        
-        let num = 1
-        
+        console.log('Performing in-memory replacements...')
         content = content.map(line => {
-            line = `E:\\TagalogWords\\Pictures\\Image${pad(num)}.jpg;${line}`
-            num++
+            line = line.replace(/[\u0000-\u001F\u007F-\u009F]/g, "")
+            let word = line.split(";")[2]
+            line = `${line};${imagePath}\\${word}.jpg`
             return line
         })
 
         content = content.join("\n")
 
         console.log('Dumping content into target file...')
-        await fs.writeFile('./words-with-tagalog-and-images.txt', content)
+        await fs.writeFile(targetFilename, content)
 
         console.log('Done. Cleaning up...')
     } catch (err) {
